e868b11c1fedb002f66cd8a1d857fbd8aa35afc3
ThomasHeniart <thomas.heniart@devfactory.com>
2018-11-14 12:51:45 -0400

    Computed properties and Watched properties

--- index.js
+++ index.js
@@ -64,6 +64,83 @@ const app7 = new Vue({
       {id: 0, text: 'Vegetables'},
       {id: 1, text: 'Meat'},
       {id: 2, text: 'Cheese'}
-    ]
+    ],
+    rawHtml: 'This should be red'
+  }
+});
+
+const app8 = new Vue({
+  el: '#app-8',
+  data: {
+    message: 'Hello world'
+  },
+  computed: {
+    revertedMessage: function() {
+      return this.message.split('').reverse().join('')
+    }
+  }
+});
+
+const app9 = new Vue({
+  el: '#app-9',
+  data: {
+    firstName: 'Foo',
+    lastName: 'Bar'
+  },
+  computed: {
+    fullName: {
+      get: function() {
+        return this.firstName + " " + this.lastName
+      },
+      set: function(fullName) {
+        var names = fullName.split(' ');
+        this.firstName = names[0];
+        this.lastName = names[names.length - 1]
+      }
+    }
+  }
+});
+
+const mustAskQuestion = 'I cannot give you an answer until you ask a question';
+
+const app10 = new Vue({
+  el: '#app-10',
+  data: {
+    question: '',
+    answer: mustAskQuestion
+  },
+  watch: {
+    question: function(newQuestion, oldQuestion) {
+      this.answer = 'Waiting for you to stop tiping...';
+      this.debouncedGetAnswer()
+    }
+  },
+  created: function() {
+    this.debouncedGetAnswer = _.debounce(this.getAnswer, 500)
+  },
+  methods: {
+    getAnswer: function() {
+      if (this.question.indexOf('?') === -1) {
+        this.answer = 'Questions usually contain a question mark. ;-)';
+        return
+      }
+
+      if (this.question.length === 0) {
+        this.answer = mustAskQuestion;
+        return
+      }
+
+      this.answer = 'Thinking...';
+
+      const self = this;
+      axios
+        .get('https://yesno.wtf/api')
+        .then(function(response) {
+          self.answer = _.capitalize(response.data.answer)
+        })
+        .catch(function(error) {
+          self.answer = 'Error, Could not reach the API. ' + error
+        })
+    }
   }
 })
